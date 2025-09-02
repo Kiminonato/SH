b76166fd3d2f0f08ec651002e9078b60d1848718
Kiminonato <oil.0924090321@gmail.com>
2025-09-02 19:58:20 +0700

    rain.cpp

--- rain.cpp
+++ rain.cpp
@@ -1,6 +1,6 @@
 void updateRainToFirebase(bool isRaining) {
   HTTPClient http;
-  String url = "https://smarthome-661b2-default-rtdb.asia-southeast1.firebasedatabase.app/Sensor/rain.json";  
+  String url = "xxx";  
 
   http.begin(client, url);  
   http.addHeader("Content-Type", "application/json");
